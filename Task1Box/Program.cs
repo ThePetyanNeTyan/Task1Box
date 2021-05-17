9e92f43fe4a442b94fce7db9af92d95b686b4dcb
typespt <65303033+typespt@users.noreply.github.com>
2021-05-17 21:37:07 +0300

    Добавьте файлы проекта.

---
+++ Task1Box/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task1Box
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
